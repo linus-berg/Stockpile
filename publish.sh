011b83fa96bfeaf8cbef701c59fcfdffc8afb8b7
Linus Berg <linus@fenix.me.uk>
2021-07-29 21:32:50 +0200

    Added specific packages for system runtimes.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-dotnet publish -p:PublishSingleFile=true -c Release --self-contained -r win-x64 -o ./Dist/Windows/
-dotnet publish -p:PublishSingleFile=true -c Release --self-contained -r linux-x64 -o ./Dist/Linux/
+dotnet publish -p:PublishSingleFile=true -c Release --self-contained true -r win-x64 -o ./Dist/Windows/
+dotnet publish -p:PublishSingleFile=true -c Release --self-contained true -r linux-x64 -o ./Dist/Linux/
